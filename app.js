d477bee8418831811ab30d97199b5491de7dde11
Kasper Hegemann <kasper.hegemann@student.ehb.be>
2021-10-15 17:15:39 +0200

    feat(socket): integrated socket.io

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 import express from 'express';
-const server = express()
+const server = express();
 const PORT = 3000;
+import {
+    Server
+} from 'socket.io';
+const io = new Server(Server);
 
 server.get('/', function (req, res) {
-    res.send('hello world')
-})
+    res.send('hello world');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
 
 server.listen(PORT, () => {
     console.log(`server listening at: http://localhost:${PORT}`);
